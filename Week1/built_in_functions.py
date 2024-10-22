5786859ea35479e093dbdb21c159a089a44e7402
keenanbernard <kbrepository@gmail.com>
2024-10-22 11:31:12 -0600

    Initial Commit

---
+++ Week1/built_in_functions.py
@@ -0,0 +1,19 @@
+print('Hello World')
+
+print('Where do you live?')
+location = input() #Used to assign values
+print('So you live in ' + location)
+
+len("Hello") # Length of the element
+
+str(55)
+
+int('55')
+
+some_int = 10
+float(some_int)
+
+def say_hello(you):
+    return 'Hello ' + you
+
+print(say_hello('Keenan.'))
---
+++ Week1/strings.py
@@ -0,0 +1,18 @@
+varA = 'Hello World' # Single line string
+print(varA)
+
+my_variable = (" This is/"
+               "to by to fit on"
+               "one line.") #MultiLine VARIABLE
+print(my_variable[3]) # Strings use 0 indexing
+
+varA = "Hello Space" # String redeclaration
+
+print(varA)
+
+a = 'Hello '
+b = 'there'
+
+print(a+b) # String concatenation
+
+print(len(a+b)) # String lenght
